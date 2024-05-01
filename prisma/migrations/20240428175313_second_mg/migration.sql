5627529b157956fd25afed05203edaeea546b212
leolucasguedes <leolucasguedes@yahoo.com.br>
2024-05-01 20:32:51 -0300

    fix: remove migrations

--- prisma/migrations/20240428175313_second_mg/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX `restaurants_address_key` ON `restaurants`;
-
--- DropIndex
-DROP INDEX `restaurants_email_key` ON `restaurants`;
-
--- DropIndex
-DROP INDEX `restaurants_phone_key` ON `restaurants`;
