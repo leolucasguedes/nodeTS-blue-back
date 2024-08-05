fdaf67ad3c95da448d2c5fab55097af98f962140
leolucasguedes <leolucasguedes@yahoo.com.br>
2024-08-05 18:49:41 -0300

    remove: migrations

--- prisma/migrations/20240805214431_add_instagram/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `instagram` to the `restaurants` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "restaurants" ADD COLUMN     "instagram" TEXT NOT NULL;
